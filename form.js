ca566fa329c162b1c827106d6da4c1a1f2d3011d
pedroperson <pedro.bom7@gmail.com>
2024-05-01 00:03:13 -0400

    did a massive cleanup of the form.js file, with some functions still a little ugly, but diminishing returns at this point. I fixed the bug

--- form.js
+++ form.js
@@ -1,192 +1,170 @@
 function formUI(formEl, store, transforms) {
-  // TODO: Need a mechanism to mark fields dirty
-
   const formData = {};
-
-  // Sample dirtyFields Set, which contains the names of the fields marked as dirty.
   const dirtyFields = new Set();
 
-  const storeFields = formEl.getAttribute("re-form").split(".").slice(1);
-  const traverseStoreFields = (val) => {
-    storeFields.forEach((f) => (val = val[f]));
-    return val;
-  };
+  store.subscribe((newValue) =>
+    handleStoreUpdate(formEl, formData, dirtyFields, transforms, newValue)
+  );
 
-  store.subscribe((v) => {
-    const changedFields = updateNonDirtyFields(
-      formData,
-      dirtyFields,
-      traverseStoreFields(v)
-    );
-
-    changedFields.forEach(([field, value]) => {
-      formEl.querySelectorAll(`[re-value="this.${field}"]`).forEach((el) => {
-        elementTransforms(el, transforms, "re-value-transform").forEach(
-          (t) => (value = t(value))
-        );
-        if (el.value !== value) {
-          el.value = value;
-        }
-      });
-
-      formEl.querySelectorAll(`[re-checked="this.${field}"]`).forEach((el) => {
-        elementTransforms(el, transforms, "re-checked-transform").forEach(
-          (t) => (value = t(value))
-        );
-
-        if (el.type === "checkbox") {
-          // force a boolean
-          value = !!value;
-          if (el.checked !== value) {
-            el.checked = value;
-          }
-        }
-
-        // ASSUMING radios have values and checkboxes don't, that is not very clear... Maybe unreasonable, need to test with more form configurations, cus this works for ours at least
-        if (el.type === "radio") {
-          if (el.value === value) {
-            if (el.checked !== true) {
-              el.checked = true;
-            }
-          }
-        }
-      });
-    });
-  });
-
-  // Update formData as the user changes the form elements
-  formEl.addEventListener("change", (event) => {
-    const el = event.target;
-    if (el.getAttribute("re-value")) {
-      let val = el.value;
-
-      elementTransforms(el, transforms, "re-value-transform-out").forEach(
-        (t) => (val = t(val))
-      );
-
-      const fields = el.getAttribute("re-value").split(".").slice(1); // skip the store's name
-      dirtyFields.add(fields.join("."));
-
-      const lastObject = fields
-        .slice(0, -1)
-        .reduce((child, f) => child[f], formData);
-
-      const lastField = fields[fields.length - 1];
-      if (lastObject[lastField] !== val) {
-        lastObject[lastField] = val;
-      }
-    } else if (el.getAttribute("re-checked")) {
-      let val = el.value;
-      // TODO: assuming checkboxes won't have values, probably not reasonable but idk
-      if (el.type === "checkbox") {
-        val = !!el.checked;
-      }
-      console.log("CEHCKED", el, el.checked, val);
-
-      elementTransforms(el, transforms, "re-checked-transform-out").forEach(
-        (t) => (val = t(val))
-      );
-
-      const fields = el.getAttribute("re-checked").split(".").slice(1); // skip the store's name
-      dirtyFields.add(fields.join("."));
-
-      const lastObject = fields
-        .slice(0, -1)
-        .reduce((child, f) => child[f], formData);
-
-      const lastField = fields[fields.length - 1];
-      if (lastObject[lastField] !== val) {
-        lastObject[lastField] = val;
-      }
-    } else {
-      console.log("non-reactive form element", el);
-      return;
-    }
-
-    console.log("NOW THE DATA", formData);
-  });
+  formEl.addEventListener("change", (event) =>
+    handleElementChange(event.target, formData, dirtyFields, transforms)
+  );
 
   if (formEl.hasAttribute("re-submit")) {
-    DOMINATOR.highjackEvent("submit", formEl, formData, () =>
+    DOMINATOR.highjackEvent("submit", formEl, formData, (e) =>
       dirtyFields.clear()
     );
   }
 
   if (formEl.hasAttribute("re-change")) {
-    DOMINATOR.highjackEvent(
-      "change",
-      formEl,
-      formData
-      //,  () => dirtyFields.clear()
+    DOMINATOR.highjackEvent("change", formEl, formData, (e) =>
+      dirtyFields.clear()
     );
-    // I with there was a callback attached where i could clear the dirty fields array
+  }
+}
+
+function handleStoreUpdate(
+  formEl,
+  formData,
+  dirtyFields,
+  transforms,
+  newValue
+) {
+  const changedFields = updateNonDirtyFields(
+    formData,
+    dirtyFields,
+    traverseElementFields(newValue, formEl, "re-form")
+  );
+
+  changedFields.forEach(([field, v]) => {
+    updateAllWithAttr("re-value", field, v);
+    updateAllWithAttr("re-checked", field, v);
+  });
+
+  function updateAllWithAttr(attr, field, value) {
+    formEl.querySelectorAll(`[${attr}="this.${field}"]`).forEach((el) => {
+      const tatt = `${attr}-transform`;
+      const v = applyTransformations(value, el, transforms, tatt);
+      updateFormElement(el, v);
+    });
+  }
+}
+
+function updateFormElement(element, value) {
+  switch (element.type) {
+    case "checkbox":
+      const isChecked = !!value;
+      if (element.checked !== isChecked) {
+        element.checked = isChecked;
+      }
+      break;
+
+    case "radio":
+      if (element.value === value) {
+        !element.checked && (element.checked = true);
+      } else {
+        element.checked && (element.checked = false);
+      }
+      break;
+
+    default:
+      if (element.value !== value) {
+        element.value = value;
+      }
+      break;
+  }
+}
+
+function applyTransformations(value, el, transforms, attr) {
+  return elementTransforms(el, transforms, attr).reduce(
+    (acc, transform) => transform(acc),
+    value
+  );
+}
+
+function handleElementChange(el, formData, dirtyFields, transforms) {
+  let attr = ["re-value", "re-checked"].find((a) => el.getAttribute(a));
+  if (!attr) {
+    console.error("Non-reactive form element", el);
+    return;
+  }
+
+  let value = el.type === "checkbox" ? el.checked : el.value;
+  value = applyTransformations(value, el, transforms, `${attr}-transform-out`);
+
+  const fields = el.getAttribute(attr).split(".").slice(1); // skip the store's name
+  const fieldPath = fields.join(".");
+
+  const lastField = fields.pop();
+  const lastObject = fields.reduce(
+    (child, f) => child[f] || (child[f] = {}), // TODO: what is this?
+    formData
+  );
+
+  if (lastObject[lastField] !== value) {
+    lastObject[lastField] = value;
+    // Tag field as dirty so that next time the store updates, we wont overwrite a field the user has changed.
+    dirtyFields.add(fieldPath);
   }
 }
 
 // This function assumes that formData and store.value are objects and can have nested structures.
 // It recursively updates formData with values from newValue that are not in dirtyFields.
-// TODO: there seems to be a bug here. When I update and submit a change from blue to red in color and then click the change mother name, nothing happens
-// Its weird, cus it works the first time flawlessly, but then after i submit some shit happens
 function updateNonDirtyFields(formData, dirtyFields, newValue, basePath = "") {
   let changedFields = [];
 
   Object.keys(newValue).forEach((key) => {
     const currentPath = basePath ? `${basePath}.${key}` : key;
-    // console.log("currentPath", currentPath);
-    // Check if the field is dirty and should be skipped.
+
+    // We skip dirty fields because we don't want to overwrite data the user has already input in case they are changing the field that got changed under them. Imagine you are writing a super long article, and then a late fetch changes the store data. I think the user would rather keep their work in most situations
     if (dirtyFields.has(currentPath)) {
-      console.log("not updating dirty field", currentPath);
       return;
     }
 
-    // Handling objects recursively, ensuring arrays and non-object values are updated directly.
-    if (Array.isArray(newValue[key])) {
-      console.log(
-        "arraycomp:",
-        formData[key],
-        newValue[key],
-        JSON.stringify(formData[key]) !== JSON.stringify(newValue[key])
-      );
-      // Simply replace the array if it's not marked as dirty
-      if (JSON.stringify(formData[key]) !== JSON.stringify(newValue[key])) {
-        formData[key] = [...newValue[key]]; // Creates a shallow copy of the array // TODO: Why shallow? what if there are objects in there?
-        // TODO: Should call the element changer from here
-        changedFields.push([currentPath, formData[key]]);
+    const newV = newValue[key];
+    const oldV = formData[key];
+
+    // Handling objects recursively
+    if (isObject(newV)) {
+      // Prepare the structure for nested updates if necessary.
+      if (!isObject(oldV)) {
+        formData[key] = {};
       }
-    } else if (
-      typeof newValue[key] === "object" &&
-      newValue[key] !== null &&
-      !Array.isArray(newValue[key])
-    ) {
-      if (
-        typeof formData[key] !== "object" ||
-        formData[key] === null ||
-        Array.isArray(formData[key])
-      ) {
-        formData[key] = {}; // Prepare the structure for nested updates if necessary.
-      }
+
       const nestedChanges = updateNonDirtyFields(
         formData[key],
         dirtyFields,
-        newValue[key],
+        newV,
         currentPath
       );
+
       changedFields = changedFields.concat(nestedChanges);
-    } else {
-      console.log(
-        "comp:",
-        formData[key],
-        newValue[key],
-        formData[key] !== newValue[key]
-      );
-      // Update the value and note the change if the value is indeed different.
-      if (formData[key] !== newValue[key]) {
-        formData[key] = newValue[key];
-        // TODO: Should call the element changer from here
+      return;
+    }
+
+    // Simply replacing arrays for now
+    if (Array.isArray(newV)) {
+      if (!equalArrays(newV, oldV)) {
+        formData[key] = structuredClone(newV);
         changedFields.push([currentPath, formData[key]]);
       }
+      return;
+    }
+
+    // Update the value and note the change if the value is indeed different.
+    if (newV !== oldV) {
+      formData[key] = newV;
+      changedFields.push([currentPath, newV]);
     }
   });
 
   return changedFields;
+}
+
+function isObject(v) {
+  return typeof v === "object" && v !== null && !Array.isArray(v);
+}
+
+function equalArrays(a, b) {
+  return JSON.stringify(a) === JSON.stringify(b);
 }
